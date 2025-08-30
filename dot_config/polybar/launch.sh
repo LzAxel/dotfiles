563b1714ae64f196478e2a904f0e00146c03b1ab
lzaxel <lzaxemial@gmail.com>
2025-08-30 15:46:55 +0300

    init dotfiles

---
+++ dot_config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar example & # Replace 'mybar' with the name of your bar section in config.ini
